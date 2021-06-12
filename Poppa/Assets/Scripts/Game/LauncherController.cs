621db6f1771fdccad5d06a8b8bb11bb0c398a129
Davie Farrell <david.farrell@outplay.com>
2021-06-12 12:49:02 +0100

    Game Path early launcher

---
+++ Poppa/Assets/Scripts/Game/LauncherController.cs
@@ -0,0 +1,38 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Copyright Davie Farrell - 2021
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherController : MonoBehaviour
+{
+    [SerializeField] private RectTransform m_ballLocator;
+    [SerializeField] private RectTransform m_viewRect;
+    
+    private Vector3 m_eulerAngles = Vector3.zero;
+    private Vector2 m_locatorPos = Vector2.zero;
+    private Vector2 m_pointerPos = Vector2.zero;
+
+    void Update()
+    {
+        FollowMouse();
+    }
+    
+    private void FollowMouse()
+    {
+        var launcherPos = m_ballLocator.position;
+        var pointerPos = Input.mousePosition;
+        
+        m_locatorPos.x = launcherPos.x;
+        m_locatorPos.y = launcherPos.z;
+        m_pointerPos.x = pointerPos.x;
+        m_pointerPos.y = pointerPos.y;
+        
+        var vectorBetween = m_pointerPos - m_locatorPos;
+
+        m_eulerAngles.z = -Vector2.SignedAngle(vectorBetween.normalized, Vector2.up);
+        m_ballLocator.eulerAngles = m_eulerAngles;
+    }
+}
---
+++ Poppa/Assets/Scripts/VFX/ScrollMaterial.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Copyright Davie Farrell - 2021
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollMaterial : MonoBehaviour
+{
+    private readonly int k_texID = Shader.PropertyToID("_MainTex");
+    
+    [SerializeField] private float m_xSpeed;
+    [SerializeField] private float m_ySpeed;
+    [SerializeField] private Image m_image;
+
+    private Vector2 m_offset;
+    
+    void Update()
+    {
+        m_offset.x = Time.time * m_xSpeed % 1f;
+        m_offset.y = Time.time * m_ySpeed % 1f;
+        
+        m_image.material.SetTextureOffset(k_texID, m_offset);
+    }
+
+    private void OnValidate()
+    {
+        m_offset = Vector2.zero;
+        Update();
+    }
+}
